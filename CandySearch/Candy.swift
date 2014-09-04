cc0c69c85edb2e1d8a2b0dc16bfe7f31b266049e
Kevin Pham <kevin@MacBook-Air.local>
2014-09-04 11:04:31 -0700

    Initial commit

---
+++ CandySearch/Candy.swift
@@ -0,0 +1,14 @@
+//
+//  Candy.swift
+//  CandySearch
+//
+//  Created by Kevin Pham on 8/29/14.
+//  Copyright (c) 2014 Kevin Pham. All rights reserved.
+//
+
+import UIKit
+
+struct Candy {
+    let category : String
+    let name : String
+}
---
+++ CandySearch/CandyTableViewController.swift
@@ -0,0 +1,159 @@
+//
+//  CandyTableViewController.swift
+//  CandySearch
+//
+//  Created by Kevin Pham on 8/29/14.
+//  Copyright (c) 2014 Kevin Pham. All rights reserved.
+//
+
+import UIKit
+
+class CandyTableViewController: UITableViewController, UISearchBarDelegate, UISearchDisplayDelegate {
+
+    var candies = [Candy]()
+    var filteredCandies = [Candy]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.candies = [Candy(category: "Chocolate", name: "Chocolate Bar"),
+                        Candy(category: "Chocolate", name: "Chocolate Chip"),
+                        Candy(category: "Chocolate", name: "Dark Chocolate"),
+                        Candy(category: "Hard", name: "Lollipop"),
+                        Candy(category: "Hard", name: "Candy Cane"),
+                        Candy(category: "Hard", name: "Jaw Breaker"),
+                        Candy(category: "Other", name: "Caramel"),
+                        Candy(category: "Other", name: "Sour Chew"),
+                        Candy(category: "Other", name: "Gummi Bear")
+        ]
+
+        self.tableView.reloadData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    /*
+    override func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
+        return 1
+    }
+    */
+
+    override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        if tableView == self.searchDisplayController!.searchResultsTableView {
+            return self.filteredCandies.count
+        } else {
+            return self.candies.count
+        }
+        
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
+        
+        var candy : Candy
+        if tableView == self.searchDisplayController!.searchResultsTableView {
+            candy = self.filteredCandies[indexPath.row]
+        } else {
+            candy = self.candies[indexPath.row]
+        }
+        
+        cell.textLabel!.text = candy.name
+        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        
+        return cell
+    }
+  
+    func filterContentForSearchText(searchText: String, scope: String = "All") {
+        // Filter the array using the filter method
+        self.filteredCandies = self.candies.filter({ (candy: Candy) -> Bool in
+            let categoryMatch = (scope == "All") || (candy.category == scope)
+            let stringMatch = candy.name.rangeOfString(searchText)
+            return categoryMatch && (stringMatch != nil)
+        })
+        
+    }
+    
+    func searchDisplayController(controller: UISearchDisplayController!, shouldReloadTableForSearchString searchString: String!) -> Bool {
+        let scopes = self.searchDisplayController!.searchBar.scopeButtonTitles as [String]
+        let selectedScope = scopes[self.searchDisplayController!.searchBar.selectedScopeButtonIndex] as String
+        self.filterContentForSearchText(searchString, scope: selectedScope)
+        return true
+    }
+    
+    func searchDisplayController(controller: UISearchDisplayController!, shouldReloadTableForSearchScope searchOption: Int) -> Bool {
+        let scope = self.searchDisplayController!.searchBar.scopeButtonTitles as [String]
+        self.filterContentForSearchText(self.searchDisplayController!.searchBar.text, scope: scope[searchOption])
+        return true
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        //self.performSegueWithIdentifier("candyDetail", sender: tableView)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if segue.identifier == "candyDetail" {
+            let candyDetailViewController = segue.destinationViewController as UIViewController
+            if self.searchDisplayController.active {
+            //if sender as UITableView == self.searchDisplayController!.searchResultsTableView {
+                let indexPath = self.searchDisplayController!.searchResultsTableView.indexPathForSelectedRow()!
+                let destinationTitle = self.filteredCandies[indexPath.row].name
+                candyDetailViewController.title = destinationTitle
+            } else {
+                let indexPath = self.tableView.indexPathForSelectedRow()!
+                let destinationTitle = self.candies[indexPath.row].name
+                candyDetailViewController.title = destinationTitle
+            }
+        }
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView!, canEditRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView!, moveRowAtIndexPath fromIndexPath: NSIndexPath!, toIndexPath: NSIndexPath!) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView!, canMoveRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
